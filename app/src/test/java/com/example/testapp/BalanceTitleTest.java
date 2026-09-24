e8d3827635465dd237fb573f34f289fbab3d19e5
Kanto <andrainaaina@gmail.com>
2026-09-24 05:53:01 +0300

    Fix balance title SMS ordering

--- app/src/test/java/com/example/testapp/BalanceTitleTest.java
+++ app/src/test/java/com/example/testapp/BalanceTitleTest.java
@@ -17,16 +17,16 @@ public class BalanceTitleTest {
                 "1 500 Ar recu de Jean 0343242318 le 21/09/26 a 08:19. Ref: 1.", 10))));
     }
 
-    @Test public void usesTransactionDateRatherThanListOrderOrReceptionDate() {
-        SmsMessage newerTransactionReceivedFirst = message(
+    @Test public void latestReceivedBalanceWinsEvenWhenItsBusinessDateIsOlder() {
+        SmsMessage smsA = message(
                 "3 000 Ar recu de LOVASOA 0343242318 le 22/09/26 a 14:10. "
-                        + "Solde: 147 479 Ar. Ref: 2.", 100);
-        SmsMessage olderTransactionReceivedLast = message(
+                        + "Solde: 10 800 Ar. Ref: 1.", 1_000);
+        SmsMessage smsB = message(
                 "3 000 Ar recu de LOVASOA 0343242318 le 21/09/26 a 08:19. "
-                        + "Solde: 82 023 Ar. Ref: 1.", 200);
+                        + "Solde: 8 800 Ar. Ref: 2.", 2_000);
 
-        assertEquals("Solde : 147 479 Ar >", BalanceTitle.from(
-                Arrays.asList(olderTransactionReceivedLast, newerTransactionReceivedFirst)));
+        assertEquals(8_800L, BalanceTitle.latestBalance(Arrays.asList(smsA, smsB)));
+        assertEquals("Solde : 8 800 Ar >", BalanceTitle.from(Arrays.asList(smsA, smsB)));
     }
 
     @Test public void formatsThousandsWithSpaces() {
@@ -35,35 +35,50 @@ public class BalanceTitleTest {
                         + "Solde: 1 000 000 Ar. Ref: 2.", 100))));
     }
 
-    @Test public void usesReceptionDateToResolveEqualTransactionTimes() {
-        SmsMessage receivedLater = message(
+    @Test public void highestRoomIdResolvesEqualReceptionDates() {
+        SmsMessage higherId = message(
                 "3 000 Ar recu de LOVASOA 0343242318 le 22/09/26 a 14:10. "
-                        + "Solde: 200 000 Ar. Ref: 3.", 300);
-        SmsMessage receivedEarlier = message(
+                        + "Solde: 200 000 Ar. Ref: 3.", 100);
+        higherId.id = 2;
+        SmsMessage lowerId = message(
                 "3 000 Ar recu de LOVASOA 0343242318 le 22/09/26 a 14:10. "
                         + "Solde: 100 000 Ar. Ref: 2.", 100);
+        lowerId.id = 1;
 
         assertEquals("Solde : 200 000 Ar >", BalanceTitle.from(
-                Arrays.asList(receivedEarlier, receivedLater)));
+                Arrays.asList(lowerId, higherId)));
     }
 
     @Test public void newestBalanceWinsAcrossWithdrawalDepositAndCreditRegardlessOfAmount() {
         SmsMessage withdrawal = message(
-                "3 000 Ar recu de LOVASOA 0343242318 le 21/09/26 a 08:19. "
-                        + "Solde: 82 023 Ar. Ref: 1.", timestamp(21, 8, 20));
+                "3 000 Ar recu de LOVASOA 0343242318 le 24/09/26 a 10:00. "
+                        + "Solde: 82 023 Ar. Ref: 1.", timestamp(24, 10, 0));
         SmsMessage deposit = message(
-                "Vous avez credite LOVASOA (0343242318) de 3 000 Ar le 22/09/26 a 14:10. "
-                        + "Bonus:24 Ar. Solde MVola : 93 599 Ar. Ref: 2",
-                timestamp(22, 14, 11));
+                "Vous avez credite LOVASOA (0343242318) de 3 000 Ar le 24/09/26 a 10:05. "
+                        + "Bonus:24 Ar. Solde MVola : 87 115 Ar. Ref: 2",
+                timestamp(24, 10, 5));
         SmsMessage credit = message(
                 "Achat de credit YAS reussi: 1 000 Ar pour 0343242318. Frais: 0 Ar. "
                         + "Bonus:24 Ar. Solde MVola : 80 869 Ar. Ref: 3",
-                timestamp(23, 10, 0));
+                timestamp(24, 10, 10));
 
         assertEquals(80_869L, BalanceTitle.latestBalance(
                 Arrays.asList(deposit, credit, withdrawal)));
         assertEquals("Solde : 80 869 Ar >", BalanceTitle.from(
                 Arrays.asList(deposit, credit, withdrawal)));
+
+        SmsMessage newerDeposit = message(
+                "Vous avez credite LOVASOA (0343242318) de 3 000 Ar le 24/09/26 a 10:15. "
+                        + "Bonus:24 Ar. Solde MVola : 93 599 Ar. Ref: 4",
+                timestamp(24, 10, 15));
+        assertEquals("Solde : 93 599 Ar >", BalanceTitle.from(
+                Arrays.asList(withdrawal, deposit, credit, newerDeposit)));
+
+        SmsMessage newestWithdrawal = message(
+                "3 000 Ar recu de LOVASOA 0343242318 le 24/09/26 a 10:20. "
+                        + "Solde: 10 800 Ar. Ref: 5.", timestamp(24, 10, 20));
+        assertEquals("Solde : 10 800 Ar >", BalanceTitle.from(
+                Arrays.asList(newerDeposit, withdrawal, newestWithdrawal, deposit, credit)));
     }
 
     @Test public void skipsNewerTransactionWithoutBalance() {
